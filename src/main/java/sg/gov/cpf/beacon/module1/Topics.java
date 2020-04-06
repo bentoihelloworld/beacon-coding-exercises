9809bc4f85831c858448c21e0f3058236e607e62
Villamor <melvin.a.villamor@accenture.com>
2020-04-06 10:06:48 +0800

    add beacon project for coding exercises

---
+++ src/main/java/sg/gov/cpf/beacon/module1/Topics.java
@@ -0,0 +1,10 @@
+package sg.gov.cpf.beacon.module1;
+
+public class Topics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/sg/gov/cpf/beacon/module2/Topic2.java
@@ -0,0 +1,5 @@
+package sg.gov.cpf.beacon.module2;
+
+public class Topic2 {
+
+}
